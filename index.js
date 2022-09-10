c6ed2b268d0c4e38871f95b6d57c09fad8257653
mohammadreza <moreh412000@gmial.com>
2022-09-10 18:50:08 +0430

    fix sizing issue and add number of grids button

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const grids = [];
 let rows = [];
 let bord = document.querySelector(".bord");
+const button = document.querySelector('button')
 let dim = 1000;
-let dimDiv = dim / 16 + "px";
-for (let i=0; i<16; i++){
-    rows[i] = document.createElement('div');
-    rows[i].style.display = "flex";
-    rows[i].style.flexShrink = 0;
-    rows[i].style.flexGrow = 0;
-    bord.appendChild(rows[i])
-    for(let j=0; j<16; j++){
-        grids[j] = document.createElement('div');
-        grids[j].style.width = dimDiv;
-        grids[j].style.height = dimDiv;
-        grids[j].style.flexShrink = 0;
-        grids[j].style.flexGrow = 0;
-        grids[j].classList.add("grid")
-        rows[i].appendChild(grids[j])
+let nGrids = 16; 
+createBord(nGrids);
+button.addEventListener('click', () => {
+    nGrids = prompt("Please enter size of grids?");
+    while (nGrids>100){
+        nGrids=prompt("above Max try again.")}
+        createBord(nGrids)}
+    )
+function createBord(nGrids){
+    rows.forEach((row) => Array.from(row.childNodes).forEach(cell => cell.remove()))
+    let dimDiv = dim / nGrids + "px";
+    for (let i=0; i<nGrids; i++){
+        rows[i] = document.createElement('div');
+        rows[i].style.display = "flex";
+        rows[i].style.flexShrink = 0;
+        rows[i].style.flexGrow = 0;
+        bord.appendChild(rows[i])
+        for(let j=0; j<nGrids; j++){
+            grids[j] = document.createElement('div');
+            grids[j].style.width = dimDiv;
+            grids[j].style.height = dimDiv;
+            grids[j].style.flexShrink = 0;
+            grids[j].style.flexGrow = 0;
+            grids[j].classList.add("grid")
+            rows[i].appendChild(grids[j])
+            }
         }
-    }
 
-rows.forEach((row) => Array.from(row.childNodes).forEach(cell => cell.addEventListener('mouseover', (e) => {
-    e.target.classList.toggle('hover')
-})))
-
-
+    rows.forEach((row) => Array.from(row.childNodes).forEach(cell => cell.addEventListener('mouseover', (e) => {
+        e.target.classList.add('hover')
+    })))
+}
 
 
