0db555013407e580680c91d1809e05c59dfb793b
mohammadreza <moreh412000@gmial.com>
2022-09-13 16:01:36 +0430

    change color from black to random

--- index.js
+++ index.js
@@ -32,8 +32,22 @@ function createBord(nGrids){
         }
 
     rows.forEach((row) => Array.from(row.childNodes).forEach(cell => cell.addEventListener('mouseover', (e) => {
-        e.target.classList.add('hover')
-    })))
+        // e.target.classList.add('hover')
+        let color = e.target.style.backgroundColor;
+        if (color==''){
+            e.target.style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+        }
+        else{
+            e.target.style.backgroundColor = `rgb(${getRGBValues(color)[0]-25}, ${getRGBValues(color)[1]-25}, ${getRGBValues(color)[2]-25})`
+        }
+        })))
 }
 
-
+function getRGBValues(color){
+    let colorV = color.substring(4,color.length-1);
+    let colorA = colorV.split(',');
+    let red = Number(colorA[0]);
+    let green = Number(colorA[1].substring(1));
+    let blue = Number(colorA[2].substring(1));
+    return [red, green, blue];
+}
